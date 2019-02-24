703003b8f1a289b3c974980a02b62489b18bbc81
Pratik Mathur <pratikmathur279@gmail.com>
2019-02-23 22:46:16 -0500

    add projects

--- src/containers/ProjectsPageBuilder/ProjectsPageBuilder.js
+++ src/containers/ProjectsPageBuilder/ProjectsPageBuilder.js
@@ -4,6 +4,7 @@ import Auxiliary from '../../hoc/Auxiliary';
 import project1 from '../../assets/Images/Embr.png';
 import project2 from '../../assets/Images/totalmortgage_website.png';
 import project3 from '../../assets/Images/burger-builder.png';
+import project4 from '../../assets/Images/teams-chatbot.png';
 import ProjectInformation from '../../components/Projects/ProjectInformation/ProjectInformation';
 
 import classes from './ProjectsPageBuilder.css';
@@ -24,13 +25,13 @@ class ProjectsPageBuilder extends Component {
                 {name: "Embr", url: project1, description: "This project was developed using ReactJS, PHP Laravel framework and hosted on Ubuntu 16.04.", heading: "Embr - Marketing Portal", isFlipped: false, index: 0},
                 {name: "TMS Website", url: project2, description: "This project was developed using ReactJS, PHP Laravel framework and hosted on Ubuntu 16.04. It uses the flexiauth library to register and manage users.", heading: "Total Mortgage Services, LLC", isFlipped: false, index: 1},
                 {name: "Burger Application", url: project3, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Burger Builder", isFlipped: false, index: 2},
-                {name: "Burger Application", url: project3, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Burger Builder", isFlipped: false, index: 3}
+                {name: "Chatbot for Microsoft Teams and Slack", url: project4, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Chatbot for Microsoft Teams and Slack", isFlipped: false, index:4}
             ],
           projects: [
                 {name: "Embr", url: project1, description: "This project was developed using ReactJS, PHP Laravel framework and hosted on Ubuntu 16.04.", heading: "Embr - Marketing Portal", isFlipped: false, index:0},
                 {name: "TMS-website", url: project2, description: "This project was developed using ReactJS, PHP Laravel framework and hosted on Ubuntu 16.04. It uses the flexiauth library to register and manage users.", heading: "Total Mortgage Services, LLC", isFlipped: false, index:1},
                 {name: "burger", url: project3, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Burger Builder", isFlipped: false, index:2},
-                {name: "burger", url: project3, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Burger Builder", isFlipped: false, index:3}
+                {name: "Chatbot for Microsoft Teams and Slack", url: project4, description: "This project was developed using ReactJS, NodeJS and MongoDB database. It was hosted using the AWS Lambda, AWS API Gateway and Serverless framework.", heading: "Chatbot for Microsoft Teams and Slack", isFlipped: false, index:4}
             ],
             description: {
                 description1: "Click on the images to view details"
