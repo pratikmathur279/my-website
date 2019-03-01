19d8b83db92fb860ba0c8d9c7793850261c2660d
Pratik Mathur <pratikmathur279@gmail.com>
2019-03-01 12:07:59 -0500

    added projects

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,16 +2,20 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cloud_logic_custom": [
+    "aws_dynamodb_all_tables_region": "us-east-1",
+    "aws_dynamodb_table_schemas": [
         {
-            "name": "amplify",
-            "endpoint": "https://3bn0p7c4f5.execute-api.us-east-1.amazonaws.com/react",
+            "tableName": "skills-react",
             "region": "us-east-1"
         }
     ],
-    "aws_content_delivery_bucket": "my-website-20190218160601-hostingbucket-react",
-    "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "http://my-website-20190218160601-hostingbucket-react.s3-website-us-east-1.amazonaws.com"
+    "aws_cloud_logic_custom": [
+        {
+            "name": "api924976bb",
+            "endpoint": "https://e7w709nryf.execute-api.us-east-1.amazonaws.com/react",
+            "region": "us-east-1"
+        }
+    ]
 };
 
 
